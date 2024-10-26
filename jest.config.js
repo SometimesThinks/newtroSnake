785251382b3e1764d8fc2efff433e66b647ad01e
SometimesThinks <ycw2859@gmail.com>
2024-10-26 23:25:22 +0900

    [chore] jest 설정

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // TypeScript 파일을 ts-jest로 트랜스파일링
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@testing-library)',
+  ],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  globals: {
+    __DEV__: true,
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
 };
